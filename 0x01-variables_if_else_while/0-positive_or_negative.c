520b361697781fe3d2f95869396277963217139c
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-09 16:33:43 +0000

    0-positive_or_negative.c commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,6 @@
 /**
  * main - Prints out a random number
  * @n: Stores a random integer value
- * 
  * Description: Stores a random number in the variable n and prints out the
  * the value.
  * Return: Returns an integer value 0
