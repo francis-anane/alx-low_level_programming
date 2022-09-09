72b8f91221d47d5a6e0be03ec20d2f431f9d41e9
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-09 16:30:53 +0000

    0-positive_or_negative.c commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,19 +9,16 @@
  * the value.
  * Return: Returns an integer value 0
  */
-
- int main(void)
+int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if(n > 0)
 		printf("%d is positive\n", n);
 	else if(n == 0)
 		printf("%d is zero\n", n);
 	else if(n < 0)
 		printf("%d is negative\n", n);
-
 	return (0);
 }
