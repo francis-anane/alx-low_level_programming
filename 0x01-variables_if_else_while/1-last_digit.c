5253fe1dde81be9c9daed83d09e406c45e816213
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-09 18:13:50 +0000

    1-last_digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
  * Return: Returns an integer value 0
  */
 int main(void)
-{	
+{
 	/*Stores a random number*/
 	int n;
 	/*Stores the last digit of n*/
@@ -20,10 +20,10 @@ int main(void)
 	last_digit = n % 10;
 	
 	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n,last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n,last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else if (last_digit < 6 && last_digit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	return (0);
 }
