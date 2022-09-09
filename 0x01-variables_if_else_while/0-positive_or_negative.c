8f5fe190b83d99a329de275c82c8bb78dbb97edf
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-09 16:19:24 +0000

    0-positive_or_negative.c commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints out a random number
+ * @n: Stores a random integer value
+ * 
+ * Description: Stores a random number in the variable n and prints out the
+ * the value.
+ * Return: Returns an integer value 0
+ */ 
+ int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if(n > 0)
+		printf("%d is positive\n", n);
+	else if(n == 0)
+		printf("%d is zero\n", n);
+	else if(n < 0)
+		printf("%d is negative\n", n);
+
+	return (0);
+}
