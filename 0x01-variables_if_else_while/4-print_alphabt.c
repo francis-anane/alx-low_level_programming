1e3969cfb731f58e73aedbb05bd3d7af61b90490
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-10 13:25:47 +0000

    4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Prints out  alphabets
+* Description: Print out the lower case alphabets from a-z without e and q
+* Return: Return 0
+*/
+int main(void)
+{
+	/*store array of lower case alphabets*/
+	char lo[] = "abcdfghijklmnoprstuvwxyz";
+	/*get size of the array*/
+	int size = 24;
+	int i;
+
+	for (i = 0; i <= size; i++)
+		putchar(lo[i]);
+	putchar('\n');
+	return (0);
+}
