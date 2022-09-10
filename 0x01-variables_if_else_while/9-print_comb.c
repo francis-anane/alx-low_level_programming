d8965508b27197a97f189c3ca9623196da35b585
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-10 16:45:50 +0000

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - Prints out  single digit numbers
+  * Description: Print out all possible combination of single digit numbers
+  * separated by commas.
+  * Return: Return 0
+  */
+int main(void)
+{
+	/*store numbers*/
+	short num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+	}
+	putchar(' ');
+	return (0);
+}
