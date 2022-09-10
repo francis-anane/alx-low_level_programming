25cf5b6fdde20855d0af50d392788a19fbbea3a5
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-10 14:16:53 +0000

    4-print_alphabt.c commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	/*store array of lower case alphabets*/
-	char lo[] = "abcdfghijklmnoprstuvwxyz\0";
+	char lo[] = "abcdfghijklmnoprstuvwxyz";
 	/*get size of the array*/
-	int size = sizeof(lo);
+	int size = sizeof(lo) / sizeof(lo[0]);
 	int i;
 
 	for (i = 0; i < size; i++)
