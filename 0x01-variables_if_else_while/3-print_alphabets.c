1cf052a4badb7469922831b43b423b21d429b563
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-10 01:27:07 +0000

    3-print_alphabets.c commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - Prints out  alphabets
+  * Description: Print out the lower and upper case alphabets from a-z/A-Z
+  * Return: Return 0
+  */
+int main(void)
+{
+	/*store array of lower case alphabets*/
+	char loc[] = "abcdefghijklmnopqrstuvwxyz";
+	/*store array of upper case alphabets*/
+	char upc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	/*get size of the array*/
+	int size = 26;
+	int i;
+
+	for (i = 0; i <= size; i++)
+	{
+		putchar(loc[i]);
+		putchar(upc[i]);
+	}
+	putchar('\n');
+	return (0);
+}
