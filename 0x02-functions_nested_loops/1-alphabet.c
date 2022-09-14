c98aad82ee8a73205dda78740932884d432218fc
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-14 14:59:06 +0000

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-putchar.c
+ * Author: Francis Ofori Anane
+ * Date:14/09/2022
+ */
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets
+ * Description: Prints out the alphabets in lower case
+ * Return: void
+ */
+void main(void)
+{
+	/*store alphabets*/
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (void);
