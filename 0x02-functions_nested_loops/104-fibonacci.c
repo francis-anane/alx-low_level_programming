533d72591bbd1608b73c2e9476882fc59ed0354e
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-14 17:58:17 +0000

    Added 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,7 +33,7 @@ int main(void)
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
 
-	for (count = 93; count < 99; count++)
+	for (counter i 93; counter < 99; counter++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
