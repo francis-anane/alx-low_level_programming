aef731df252e00a773b81b87848486b91b64a991
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-14 16:49:00 +0000

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-alphabet.c
+ * Author: Francis Ofori Anane
+ * Date: 14/09/2022
+ */
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase, followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+	char c;
+
+	while (counter <= 10 )
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		counter++;
+		_putchar('\n');
+	}
+}
