f7ad46b6fd669485d474849391988795c78e12fa
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-14 14:25:09 +0000

    Updated commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-putchar.c
+ * Author: Francis Ofori Anane
+ * Date:13/09/2022
+ */
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: Prints out the string _putchar
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	/*store some string*/
+	char s[] = "_putchar";
+	int size = sizeof(s);
+	int i;
+
+	for (i = 0;  i <= size;  i++)
+		putchar(s[i]);
+	putchar('\n');
+	return (0);
+}
