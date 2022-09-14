c1f12ea507d2c365b1a8470323d189cf329d3b5a
FrancisOforiAnane <22830398+Abstrat22@users.noreply.github.com>
2022-09-14 12:16:28 -0400

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
+/*
+ * File: 1-alphabet.c
+ * Author: Francis Ofori Anane
+ * Date: 14/09/2022
+ */
 
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+                _putchar(c);
+
+        _putchar('\n');
+}
