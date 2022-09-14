57c5df4b90a933499154a6af6e76b06bf10fb63d
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-14 17:29:53 +0000

    Updated commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * File: 10-add.c
+ * Author: Francis Ofori Anane
+ * Date: 14/09/2022
+ */
+
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num0: The first integer to be added.
+ * @num1: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num0, int num1)
+{
+	return (num0 + num1);
+}
