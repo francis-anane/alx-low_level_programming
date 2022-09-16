61a456601891524dc93faae5f4eec6c0864cbdc3
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-16 20:30:37 +0000

    Update commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,11 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 98;
+
 	/*while (i < 10)*/
 	 /*{*/
 	 /*	putchar(i);*/
 	 /*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
