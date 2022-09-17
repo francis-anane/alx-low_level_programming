e812ce48d06efc8caf932c65a797e1536ada3207
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-17 00:35:38 +0000

    5-more_numbers.c commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers -  prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * more_numbers -  prints 10 times the numbers, from 0 to 14, followed 
+ * by a new line.
  *
  * Return: no return
  */
