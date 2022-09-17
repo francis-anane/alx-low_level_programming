36efbe1175a05e1a3a2adab67e751b7e85e59f5a
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-17 00:40:04 +0000

    Update commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,8 +5,8 @@
  * main - FizzBuzz
  *
  * Description: prints the numbers from 1 to 100, followed by a new line.
- * But for multiples of three print Fizz instead of the number and for the 
- * multiples of five print Buzz. For numbers which are multiples of both 
+ * But for multiples of three print Fizz instead of the number and for the
+ * multiples of five print Buzz. For numbers which are multiples of both
  * three and five print FizzBuzz.
  * Return: always 0
  */
