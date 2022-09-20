9a6e1645347bab5be71f4e3c810f50825246c250
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-20 14:45:35 +0000

    Add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Francis Ofori Anane
+ * Date: 20/09/2022
+ */
+
+/**
+  * reset_to_98 - Takes a pointer to an int as parameter and updates
+  * the value it points to to 98.
+  *
+  * @n: The said pointer
+  */
+
+void reset_to_98(int *n)
+{
+	&n = 98;
+}
