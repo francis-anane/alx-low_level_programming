90581f97d91f62b8c0aa0b0eeda71b59deb7a6a7
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-21 17:09:53 +0000

    Add commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 21/09/2022
+ */
+
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+	int i = 0;
+	int j;
+	char c[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == c[j] || str[i] - 32 == c[j])
+				str[i] = j + '0';
+		}
+
+		i++;
+	}
+
+	return (str);
+}
