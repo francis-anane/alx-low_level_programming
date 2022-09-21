910d95ecc8a81ec77eb7db2fb6b9d56cef2b57ae
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-21 17:19:09 +0000

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||str[i - 1] == '\n' ||
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
 		    str[i - 1] == ',' ||
 		    str[i - 1] == ';' ||
 		    str[i - 1] == '.' ||
