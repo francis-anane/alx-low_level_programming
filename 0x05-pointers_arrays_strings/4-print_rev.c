db14341d71c3d9cd1719d909ad91212d83049a6d
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-21 00:55:21 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,10 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int size = sizeof(s) ;
+	int i;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
+	for (i = size; i >= 0 ; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
