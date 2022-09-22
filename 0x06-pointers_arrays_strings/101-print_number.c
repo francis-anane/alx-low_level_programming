b89d1451062751dbb1ddad0d1225cfa8c96de22e
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-22 23:30:42 +0000

    Add commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,15 +11,17 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int m;
+	m = n;
+	if (m < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -m;
 
 	}
 
-	if ((n / 10) > 0)
-		print_number(n / 10);
+	if ((m / 10) > 0)
+		print_number(m / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(m % 10 + '0');
 }
