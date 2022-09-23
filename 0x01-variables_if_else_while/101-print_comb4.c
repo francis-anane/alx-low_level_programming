4dd758a2b1e345b0ab80b70c28c543d2eed3abf5
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-23 19:25:03 +0000

    101-print_comb4.c commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+/**
+  * main - entry point
+  * Desc: Prints all possible different combinations of three digits
+  * Return: 0 (success)
+  */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+		for (j = 48; j < 58; j++)
+			for (k = 48; k < 58; k++)
+			
+			if (i != j  && i < j && j != k && j < k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (j == 57 && i == 56)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+
+	putchar('\n');
+	return (0);
+}
