40e37ba83566d2abd0797eacae302ea686ec4781
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-23 17:40:30 +0000

    Add commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+/**
+  * main - entry point
+  * Desc: Prints all possible different combinations of two digits
+  * Return: 0 (success)
+  */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+		for (j = 48; j < 58; j++)
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (j == 57 && i == 56)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+	return (0);
+}
