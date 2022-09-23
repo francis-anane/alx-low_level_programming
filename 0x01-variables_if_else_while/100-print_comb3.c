a81bf2eb829d7d46fddb8b825774bb936d05d579
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-23 17:53:09 +0000

    Add commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,10 +17,11 @@ int main(void)
 				putchar(i);
 				putchar(j);
 
-				if (j == 57 && i == 57)
+				if (j == 57 && i == 56)
 					break;
 				putchar(',');
 				putchar(' ');
 			}
+	putchar('\n');
 	return (0);
 }
