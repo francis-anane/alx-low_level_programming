2f02c7dded89c20c269e09540c28091065a268c5
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-28 00:15:47 +0000

    Add 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,10 +6,10 @@
 #include "main.h"
 
 /**
-  * _strspn - Gets the length of a prefix substring..
-  * @s: The string contain substring
-  * @accept: The substring 
-  * Return: The number of bytes in the initial segment 
+  * _strspn - Gets the length of a prefix substring.
+  * @s: The string contain substring.
+  * @accept: The substring
+  * Return: The number of bytes in the initial segment
   * of @s which consist only of bytes from accept
   */
 
@@ -17,17 +17,17 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes;
 
-	while(*s)
+	while (*s)
 	{
-		while(*accept)
+		while (*accept)
 		{
-			if(*s == *accept)
+			if (*s == *accept)
 			{
 				bytes++;
 				break;
 			}
-			
-			else if(*accept + 1 == '\0')
+			else if (*accept + 1 == '\0')
+
 				return (bytes);
 
 			accept++;
