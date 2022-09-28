9a51c5d8fcde46d2632a2d21856620e60495a3a6
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-28 00:47:18 +0000

    Add 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,23 +16,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
+	int i = 0;
 
 	while (*s)
 	{
-		while (*accept)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*s == *accept)
+			if (*s == accept[i])
 			{
 				bytes++;
 				break;
 			}
-			else if (*accept + 1 == '\0')
 
+			else if (accept[i + 1] == '\0')
 				return (bytes);
-
-			accept++;
-
-
 		}
 
 		s++;
