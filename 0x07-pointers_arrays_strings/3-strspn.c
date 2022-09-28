3a47625dbf99cdd886092115de8492e8620ca032
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-28 00:09:12 +0000

    Add 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,43 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 27/09/2022
+ */
+
+#include "main.h"
+
+/**
+  * _strspn - Gets the length of a prefix substring..
+  * @s: The string contain substring
+  * @accept: The substring 
+  * Return: The number of bytes in the initial segment 
+  * of @s which consist only of bytes from accept
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes;
+
+	while(*s)
+	{
+		while(*accept)
+		{
+			if(*s == *accept)
+			{
+				bytes++;
+				break;
+			}
+			
+			else if(*accept + 1 == '\0')
+				return (bytes);
+
+			accept++;
+
+
+		}
+
+		s++;
+
+	}
+
+	return (bytes);
+}
