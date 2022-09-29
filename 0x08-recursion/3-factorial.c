41b7b4b760dccfbde2514e1c47c91e049f684ec5
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-29 22:18:40 +0000

    Add commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 28/09/2022
+ */
+
+#include "main.h"
+
+/**
+  * fact - Find the factorial of a number
+  * @n: Number to find factorial of
+  * Return: Factorial of the number
+  */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+
+	return (n * factorial(n-1));
+	
+}
