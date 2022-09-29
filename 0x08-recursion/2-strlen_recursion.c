83aa70e46b013c79ddd14b53c12ecd3a16039443
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-29 21:51:21 +0000

    Add 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 28/09/2022
+ */
+
+#include "main.h"
+
+/**
+  * _strlen_recursion -  returns the length of a string.
+  * @s: The string
+  * Return: Length of string @s.
+  */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	
+	if (*s != '\0')
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	
+	}
+	return(i);
+}
