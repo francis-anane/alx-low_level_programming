ce9ec828055d9f64a88c687d8894a7c80cacab8f
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-30 18:13:35 +0000

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 30/09/2022
+ */
+
+/**
+  * main - entry point
+  * Desc: Prints the name of the program followed by a new line.
+  * @argc: command line argument count.
+  * @argv: An array of command line arguments.
+  * Return: 0 (success)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
