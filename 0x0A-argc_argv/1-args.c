dbd0ff52c13b4e78440a2f47132bbbe77607b5eb
Francis Ofori Anane <ofori.anane@gmail.com>
2022-09-30 18:36:37 +0000

    Add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 30/09/2022
+ */
+
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Desc: Prints the number of arguments passed at command line.
+  * @argc: command line argument count.
+  * @argv: An array of command line arguments.
+  * Return: 0 (success)
+  */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc-1);
+
+	return (0);
+}
