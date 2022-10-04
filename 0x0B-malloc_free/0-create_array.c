48111d58a744b64e563c3434d05cd951cf2b7b08
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-04 10:08:32 +0000

    Add commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,41 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 4/10/2022
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of 
+ * chars, and initializes it with 
+ * a specific char.
+ *
+ * @size: The size of the array.
+ * @c: The character to initialize 
+ * the array.
+ *
+ * Return: NULL if size = 0, or 
+ * A pointer to the array, NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(c)*size);
+	
+	for (i = 0; i < size; i++)
+
+		array[i] = c;
+
+	return (array);
+
+	
+
+	
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+# My alx course work, task 0x0B. C - malloc, free
