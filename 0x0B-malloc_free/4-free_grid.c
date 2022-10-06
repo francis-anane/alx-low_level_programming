603752cd021890e6953898a74b934eeb29693cea
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-06 05:06:45 +0000

    Add commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,7 +8,7 @@
 
 /**
  * free_grid - Frees a 2-dimensional array of integers.
- * @grid: The 2-dimensional array of integers to be freed.
+ * @grid: The array of integers to be freed.
  * @height: The height of grid.
  */
 void free_grid(int **grid, int height)
