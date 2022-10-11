94ea0e5fe625fc34ae0dc1440045722221d5c52e
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-11 05:05:59 +0000

    Add commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 11/10/2022
+ */
+
+#include "dog.h"
+
+/**
+  * init_dog - initialize a variable of type dog
+
+  * @d: The dog.
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The dog's owner.
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{	
+	d->name = name;
+	d->age = age;
+	(*d).owner = owner;
+}
