5620237c5847968f7f1079ce6d4089e469c46356
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-12 15:11:17 +0000

    Add commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 11/10/2022
+ */
+
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Print a struct dog.
+ *
+ * @d: A dog structure.
+ */
+
+void print_dog(struct dog *d)
+{
+	if(d == NULL)
+
+		return;
+
+	if ((*d).name != NULL)
+
+		printf("Name: %s\n", (*d).name);
+	else
+		printf("Name: (nill)\n");
+
+	printf("Age: %f\n", (*d).age);
+
+	if((*d).owner != NULL)
+
+		printf("Owner: %s\n", (*d).owner);
+	else
+		printf("Owner: (nill)\n");
+}
