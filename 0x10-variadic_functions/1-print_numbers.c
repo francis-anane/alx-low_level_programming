aa17c1f9bda88d648ee59941a2ff18353c9469f2
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-14 09:18:02 +0000

    Add commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -30,11 +30,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(ap, int);
 		printf("%d", num);
-		if (separator != NULL && i != n-1)
+		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
 
-	va_end(ap);
-
 	printf("\n");
+
+	va_end(ap);
 }
