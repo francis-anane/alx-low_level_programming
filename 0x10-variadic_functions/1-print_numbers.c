65ed817470b46d596ff27ef488c26431d84064f8
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-14 09:11:45 +0000

    Add commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,40 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 14/10/2022
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ *
+ * @separator: The string to be printed between numbers.
+ * @n: The last argument of the list of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num = 0;
+
+	va_list ap;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ap, int);
+		printf("%d", num);
+		if (separator != NULL && i != n-1)
+			printf("%s", separator);
+	}
+
+	va_end(ap);
+
+	printf("\n");
+}
