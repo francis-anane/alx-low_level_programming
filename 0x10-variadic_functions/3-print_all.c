e3c68c25f378a8a0e0b2185d2f6915c91f000c98
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-15 02:18:29 +0000

    Add commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,49 +2,40 @@
  * Author: Francis Ofori Anane
  * Date: 14/10/2022
  */
-
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * print_all - Prints any data type given as argument.
  *
  * @format: The format of the data type.
  */
-
 void print_all(const char * const format, ...)
 {
 	int i = 0, len = strlen(format);
 	char c, *s;
 	int d;
 	double f;
-
 	va_list ap;
 
 	va_start(ap, format);
-
 	while (i < len)
 	{
-		switch(format[i])
+		switch (format[i])
 		{
-
 		case 'c':
 			c = va_arg(ap, int);
 			printf("%c", c);
 			break;
-
 		case 'i':
 			d = va_arg(ap, int);
 			printf("%d", d);
 			break;
-
 		case 'f':
 			f = va_arg(ap, double);
-			printf ("%f", f);
+			printf("%f", f);
 			break;
-
 		case 's':
 			s = va_arg(ap, char*);
 			if (s == NULL)
@@ -54,20 +45,11 @@ void print_all(const char * const format, ...)
 		default:
 			break;
 		}
-
-		while((i < len - 1) && (format[i]
-			== 'c' || format[i] ==
-		       'i' || format[i] == 'f'
-		      || format[i] == 's'))
-		{
+		if ((i < len - 1) && (format[i] == 'c' || format[i] == 'i' ||
+				      format[i] == 'f' || format[i] == 's'))
 			printf(", ");
-			break;
-		}
-
 		i++;
 	}
-
 	printf("\n");
-
 	va_end(ap);
 }
