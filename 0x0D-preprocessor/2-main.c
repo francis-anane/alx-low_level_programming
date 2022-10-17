8f11db609214f638e9587676bced2dc38f563199
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-17 01:14:42 +0000

    Add commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 17/10/2022
+ */
+
+/**
+ * main - entry point.
+ * Description: Print the name of the sourse file.
+ *
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
