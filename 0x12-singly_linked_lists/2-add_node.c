1e521f97e5bfa4c8eddfc7776e5f4d16c5d8ee79
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-21 21:43:15 +0000

    Added 2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,7 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 
 	new_node->str = strdup(str);
-	new_node->len =strlen(str);
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
