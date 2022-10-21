f29ccb6a84f3e5b88ad1a2e8339e6494b8333822
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-21 19:56:01 +0000

    initial commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,9 +17,10 @@
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
+
 	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
