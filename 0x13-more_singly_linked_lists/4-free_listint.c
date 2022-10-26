99a779264d5140697f915c6cdaf46a88a94c350f
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-26 17:25:44 +0000

    Add commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,27 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 26/11/2022
+ */
+
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint - Frees a listint_t list.
+ *
+ * @head: The list to free.
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		free(head->n)
+		free(head);
+		head = head->next;
+	}
+}
