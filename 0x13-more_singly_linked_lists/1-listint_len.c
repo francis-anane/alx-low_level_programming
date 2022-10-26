cd3ebf6b1f52fcaa1f1a3d1a7154b9acc6b2d289
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-26 16:49:33 +0000

    Add commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,28 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 21/10/2022
+ */
+
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * listint_len - Returns the number of
+ * elements in a linked listint_t list.
+ *
+ * @h: The list to return the elements of.
+ * Return: The number of elements of the list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		elements += 1;
+	}
+	return (elements);
+}
