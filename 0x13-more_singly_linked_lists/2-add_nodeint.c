5f19a863eefe6c49e3fdea5b5b845c9f93343215
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-26 17:03:24 +0000

    Add commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,30 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 21/10/2022
+ */
+
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_nodeint - Adds a new node at the
+ * beinning of a list_t list.
+ *
+ * @head: The pointer to the head node of the list.
+ * @n: Data for the new node.
+ * Return: The address of the new node.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
