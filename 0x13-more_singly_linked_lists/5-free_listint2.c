908a377e9a72c09a3c2fab8b3b8273b6199009e6
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-27 01:53:40 +0000

    Update commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 26/11/2022
+ */
+
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint - Frees a listint_t list.
+ *
+ * @head: The list to free.
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+}
