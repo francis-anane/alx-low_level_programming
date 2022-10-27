3803254fdde71e7b40ac21b3dcd76dc185a60a07
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-27 01:48:44 +0000

    Update commit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -15,15 +15,15 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *node
+	listint_t *node;
 
 	if (head == NULL)
 		return;
 
 	while (head != NULL)
 	{
-		free(head);
 		node = head->next;
+		free(head);
 		head = node;
 	}
 }
