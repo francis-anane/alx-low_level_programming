0c2948d6cfcc81563446fbda5b556365111aac5b
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-29 20:54:45 +0000

    Update 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,40 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 29/10/2022
+ */
+
+#include <string.h>
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ *
+ * @n: The number to print binary of.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int msk_val = 32768;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+        {
+                _putchar('1');
+                return;
+        }
+
+	while (msk_val > 0)
+	{
+		if (n & msk_val == 0)
+			_putchar('0');
+
+		if (n & msk_val == 1)
+                        _putchar('1');
+
+		msk_val = msk_val >> 1;
+	}
+
+}
