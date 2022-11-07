2f88f67089b9e4c8ab5002a55ff0843347141d1d
Francis Ofori Anane <ofori.anane@gmail.com>
2022-11-07 13:52:59 +0000

    initial commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+/*
+ * Author: Francis Ofori Anane
+ * Date: 7/11/2022
+ */
+
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * read_textfile - Reads a text file and
+ * print it to the POSIX standard output.
+ *
+ * @filename: A pointer to the file path.
+ * @letters: The number of
+ * charactes to read and print.
+ * Return: Number of characters read on success, or 0, if the file could not be
+ * open or read, if filename is NULL or
+ * if write fails and could not write
+ * the expected bytes.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t bytes;
+	char *buff;
+
+	/*allocate memory for text buffer*/
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (-1);
+
+	if (filename == NULL)
+		return (0);
+
+	/*open file for reading*/
+	fd = open(filename, O_RDONLY);
+	bytes = read(fd, buff, letters);
+
+	/*write text to standard output*/
+	bytes = write(STDOUT_FILENO, buff, letters);
+
+	if (bytes != letters)
+		return (0);
+
+	close(fd);
+	free(buff);
+	return (bytes);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+# My alx course work, task 0x15. C - File I/O
