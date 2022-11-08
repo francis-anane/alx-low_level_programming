7803967d3017a53c076069e56d4acaa12d30ab92
Francis Ofori Anane <ofori.anane@gmail.com>
2022-11-08 12:18:49 +0000

    Update commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,11 +25,9 @@ int create_file(const char *filename, char *text_content)
 	char *buff;
 
 	size = strlen(text_content);
-
 	buff = malloc(size + 1);
 	if (buff == NULL)
 		return (-1);
-
 	if (filename == NULL)
 		return (-1);
 
