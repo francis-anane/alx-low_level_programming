ed6f2dd7e401e31433d4c87a031d39e616c7cf09
Francis Ofori Anane <ofori.anane@gmail.com>
2023-04-02 13:03:40 +0000

    Update commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,8 +21,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = NULL;
+
+	if(*head == NULL)
+		*head = new_node;
+	else
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
 
 	return (new_node);
 }
