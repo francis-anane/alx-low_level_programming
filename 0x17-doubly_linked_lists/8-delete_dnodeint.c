2ffecf58f2296e8082cbf69afcd3a62163c9b439
Francis Ofori Anane <ofori.anane@gmail.com>
2023-06-01 18:57:40 +0000

    Updated commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -41,7 +41,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				prev = tmp;
 			tmp = tmp->next;
 		}
-		/*node = tmp;*/
 		prev->next = tmp->next;
 		tmp->next->prev = prev;
 	}
