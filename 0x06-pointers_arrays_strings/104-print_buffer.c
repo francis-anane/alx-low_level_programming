0f5aa0d19075e77e4e926ac080084e16d2a57221
Francis Ofori Anane <ofori.anane@gmail.com>
2023-06-27 16:46:19 +0000

    Add files

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
 /**
+ * print_ascii - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: the string to print
+ * @start: the starting position
+ * @end: the end position
+ */
+void print_ascii(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!(((int)ch >= 32) && ((int)ch <= 126)))
+			ch = 46; /*ascii for '.' */
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_hexes - print hex values for string b in formatted form
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void print_hexes(char *b, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/**
  * print_buffer - Prints the content of a buffer
  * @b: Pointer to the buffer
  * @size: Size of the buffer
@@ -8,44 +51,18 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int start, end;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		printf("\n");
-		return;
-	}
-
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x:  ", i);
-
-		for (j = i; j < i + 10; j += 2)
+		for (start = 0; start < size; start += 10)
 		{
-			if (j < size)
-				printf("%02x", b[j]);
-			else
-				printf("  ");
-
-			if (j + 1 < size)
-				printf("%02x ", b[j + 1]);
-			else
-				printf("   ");
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			print_hexes(b, start, end);
+			print_ascii(b, start, end);
+			printf("\n");
 		}
-
-		printf(" ");
-
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
-			{
-				if (b[j] >= 32 && b[j] <= 126)
-					printf("%c", b[j]);
-				else
-					printf(".");
-			}
-		}
-
+	} else
 		printf("\n");
-	}
 }
