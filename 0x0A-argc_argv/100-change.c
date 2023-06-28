30ecf5f1e66b3cfeac67259d52e584ddf079bea9
Francis Ofori Anane <ofori.anane@gmail.com>
2023-06-28 00:22:02 +0000

    Update file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,18 +9,19 @@
  */
 int get_min_coins(int cents)
 {
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins, min_coins = 0, i, count;
+
 	if (cents <= 0)
 		return (0);
 
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int min_coins = 0;
+	num_coins = sizeof(coins) / sizeof(coins[0]);
 
-	for (int i = 0; i < num_coins; i++)
+	for (i = 0; i < num_coins; i++)
 	{
 		if (cents >= coins[i])
 		{
-			int count = cents / coins[i];
+			count = cents / coins[i];
 
 			min_coins += count;
 			cents -= count * coins[i];
@@ -39,21 +40,22 @@ int get_min_coins(int cents)
  */
 int main(int argc, char *argv[])
 {
+	int cents, min_coins;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
-
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	int min_coins = get_min_coins(cents);
+	min_coins = get_min_coins(cents);
 
 	printf("%d\n", min_coins);
 
