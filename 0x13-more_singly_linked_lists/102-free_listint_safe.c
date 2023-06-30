7eee2b8806ac7092cc1a4634f4b1948a9125cedf
Francis Ofori Anane <ofori.anane@gmail.com>
2023-06-30 12:09:50 -0400

    Update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -6,47 +6,26 @@
  *
  * Return: size of the list that was freed.
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *slow = *h, *fast = *h, *loop_start = NULL;
+	listint_t *current = *h;
 	size_t node_count = 0;
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	while (current != NULL)
 	{
-		slow = slow->next, fast = fast->next->next;
-		if (slow == fast)
+		listint_t *temp = current->next;
+
+		free(current);
+		node_count++;
+		current = temp;
+
+		if (current == *h)
 		{
-			loop_start = slow;
+			*h = NULL;
 			break;
 		}
 	}
-	if (loop_start != NULL)
-	{
-		listint_t *temp = loop_start;
 
-		do {
-			temp = temp->next, node_count++;
-		} while (temp != loop_start);
-		temp = *h;
-		while (loop_start->next != temp)
-		{
-			listint_t *next_node = temp->next;
-
-			free(temp), temp = next_node, node_count--;
-		}
-		free(temp), node_count--;
-	}
-	else
-	{
-		listint_t *current = *h;
-
-		while (current != NULL)
-		{
-			listint_t *next_node = current->next;
-
-			free(current), current = next_node, node_count++;
-		}
-	}
-	*h = NULL;
 	return (node_count);
 }
