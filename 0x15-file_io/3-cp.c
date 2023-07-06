58f7815b0275b34bdff4ab517407b99ba471cede
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-06 17:32:53 -0400

    Update file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,12 +3,27 @@
  * Date: 15/02/2023
  */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <fcntl.h>
+#include "main.h"
+/**
+ * access_err - checks if files can be opened.
+ * @fd_from: The file_from.
+ * @fd_to: file_to.
+ * @av: argument vector.
+ */
+
+void access_err(int fd_from, int fd_to, char *av[])
+{
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+}
 
 /**
  * main - Entry point.
@@ -20,46 +35,42 @@
  * Return: 0 (success)
  */
 
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
-	int fd_from, fd_to, rd;
-	char *buff;
+	int fd_from, fd_to;
+	ssize_t rbytes, wbytes;
+	char buff[1024];
 
-	if (ac < 3 || ac > 3)
+	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	if (access(av[1], F_OK | R_OK) != 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			av[1]);
-		exit(98);
-	}
-
-	buff = malloc(1024);
 
 	fd_from = open(av[1], O_RDONLY);
-	rd = read(fd_from, buff, 1024);
-	if (close(fd_from) < 0)
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	access_err(fd_from, fd_to, av);
+
+	rbytes = 1024;
+	while (rbytes == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from
-);
+		rbytes = read(fd_from, buff, 1024);
+		if (rbytes == -1)
+			access_err(-1, 0, av);
+		wbytes = write(fd_to, buff, rbytes);
+		if (wbytes == -1)
+			access_err(0, -1, av);
+	}
+
+	if ((close(fd_from)) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-	if (access(av[2], F_OK) == 0)
-
-		fd_to = open(av[2], O_WRONLY | O_TRUNC);
-	else
-		fd_to = creat(av[2], 00664);
-
-	write(fd_to, buff, rd);
-
-	if (close(fd_to) < 0)
+	if ((close(fd_to)) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
