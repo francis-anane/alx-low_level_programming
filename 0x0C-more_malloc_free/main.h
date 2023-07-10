b9bf73b5c2556fbafbd9f4f192590fc6513bf5f7
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-10 12:10:24 -0400

    Update file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,16 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int _putchar(char c);
+void _print(char *digits, int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmedb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
