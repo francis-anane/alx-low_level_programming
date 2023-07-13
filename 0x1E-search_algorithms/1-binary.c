3b39d655611bdfd30c2d90223977fb8ab2560a5c
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-13 16:28:08 -0400

    Add files

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ * @array: Pointer to the first element of the array
+ * @size: size of the array
+ * @value: The value to search
+ * Return: Index of the value or, -1 on failure
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int start = 0, end = size - 1, mid, i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (start <= end)
+	{
+		mid = start + (end - start) / 2;
+
+		printf("Searching in array");
+		for (i = start; i <= end; i++)
+			printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+		printf("\n");
+
+		if (array[mid] == value)
+			return (int)mid;
+
+		else if (array[mid] < value)
+			start = mid + 1;
+		
+		else
+			end = mid - 1;
+	}
+	return (-1);
+}
